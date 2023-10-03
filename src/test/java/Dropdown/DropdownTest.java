9b0c7ac86f849ab30d4d776282a5c5d0562f26eb
Aaron Flynn <azza_554@hotmail.com>
2023-10-03 15:26:41 +0100

    Chapter 4.3 Lesson

---
+++ src/test/java/Dropdown/DropdownTest.java
@@ -0,0 +1,22 @@
+package Dropdown;
+
+import base.BaseTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DropdownTest extends BaseTest {
+
+    @Test
+    public void testSelectOption(){
+        String option = "Option 1";
+
+        var dropDownPage = homePage.clickDropdown();
+        dropDownPage.selectFromDropdown(option);
+
+        var selectedOptions = dropDownPage.getSelectedOptions();
+        assertEquals(selectedOptions.size(), 1, "Incorrect number of options selected.");
+        assertTrue(selectedOptions.contains(option), "Option 1 not selected.");
+    }
+}
