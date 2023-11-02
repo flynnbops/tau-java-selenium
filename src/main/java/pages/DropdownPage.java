9076fbc02ac6382ef3c654283feda3952a69bf41
flynnnbops <azza_554@hotmail.com>
2023-11-02 15:40:55 +0000

    Chapter 10 exercise: Change dropdown list into multiple selection

--- src/main/java/pages/DropdownPage.java
+++ src/main/java/pages/DropdownPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
@@ -25,6 +26,14 @@ public class DropdownPage {
     public List<String> getSelectedOptions(){
         List<WebElement> selectedElements = findDropdown().getAllSelectedOptions();
         return selectedElements.stream().map(e->e.getText()).collect(Collectors.toList());
+    }
+
+    /**
+     * Change the dropdown list into a multiple select option.
+     */
+    public void changeToMultiple(){
+        String script = "arguments[0].setAttribute('multiple', '')";
+        ((JavascriptExecutor)driver).executeScript(script, findDropdown());
     }
 
     private Select findDropdown(){
--- src/test/java/javascript/JavascriptTest.java
+++ src/test/java/javascript/JavascriptTest.java
@@ -3,6 +3,7 @@ package javascript;
 import base.BaseTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JavascriptTest extends BaseTest {
 
@@ -16,5 +17,21 @@ public class JavascriptTest extends BaseTest {
     @DisplayName("Use JavaScript to scroll to the fifth paragraph of text")
     public void scrollToFifthParagraph(){
         homePage.clickInfiniteScroll().scrollToParagraph(5);
+    }
+
+    @Test
+    @DisplayName("Use JavaScript to change a dropdown attribute")
+    public void changeDropdownToMultiple(){
+        var dropdown = homePage.clickDropdown();
+
+        dropdown.changeToMultiple();
+        dropdown.selectFromDropdown("Option 1");
+        dropdown.selectFromDropdown("Option 2");
+
+        // Verify both options are selected
+        var selectedOptions = dropdown.getSelectedOptions();
+        assertEquals(2,selectedOptions.size(),"both options where not selected");
+        assertEquals("Option 1",selectedOptions.get(0),"Option 1 was not the first selected option");
+        assertEquals("Option 2",selectedOptions.get(1),"Option 2 was not the second selected option");
     }
 }
