641930adda25f574116069541e4f43d48a208c34
flynnnbops <azza_554@hotmail.com>
2023-11-03 16:45:23 +0000

    Chapter 13 + 14: Event listeners and webDriver extras.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,24 +2,37 @@ package base;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import pages.HomePage;
+import utils.EventReporter;
 import utils.WindowManager;
 
 public class BaseTest {
-    private WebDriver driver;
+    private EventFiringWebDriver driver;
     protected HomePage homePage;
 
+    private ChromeOptions getChromeOptions(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("disable-infobars");
+        return options;
+    }
+
+    private void setCookie(){
+        Cookie cookie = new Cookie.Builder("name", "123")
+                .domain("t")
+                .build();
+        driver.manage().addCookie(cookie);
+    }
     @BeforeEach
     public void setUp() {
-//        System.getProperty("webdriver.chrome.driver", "resources/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing");
         System.getProperty("webdriver.chrome.driver", "resources/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("headless");
-
-        driver = new ChromeDriver(options);
+        driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
+        driver.register(new EventReporter());
+        setCookie();
         driver.get("https://the-internet.herokuapp.com/");
         homePage = new HomePage(driver);
     }
